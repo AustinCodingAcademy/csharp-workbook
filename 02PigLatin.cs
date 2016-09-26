ee3d027bbd62db8352516bfd5040eb77febca08c
Kevin Colten <kevincolten@gmail.com>
2016-09-25 23:10:42 -0500

    Update and rename 01PigLatin.cs to 02PigLatin.cs

---
+++ 02PigLatin.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("What's the word?");
+        string word = Console.ReadLine();
+        Console.WriteLine(translateWord(word));
+    }
+    
+    public static string translateWord(string word)
+    {
+        // Your code here
+        return word;
+    }
+}

d81e0c9024078e76a85e667f44e76af0c63be5a0
Kevin Colten <kevincolten@gmail.com>
2016-09-25 22:46:22 -0500

    Create 02RockPaperScissors.cs

---
+++ 02RockPaperScissors.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter hand 1:");
+        string hand1 = Console.ReadLine().ToLower();
+        Console.WriteLine("Enter hand 2:");
+        string hand2 = Console.ReadLine().ToLower();
+        Console.WriteLine(CompareHands(hand1, hand2));
+    }
+    
+    public static string CompareHands(string hand1, string hand2)
+    {
+        return hand1 + " " + hand2;
+    }
+}

905b9c26041944637947fe9117a3a6fdfcfa1f82
Kevin Colten <kevincolten@gmail.com>
2018-06-07 19:15:20 -0500

    Server

---
+++ Server/Server.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory = Directory.GetCurrentDirectory() + "/html";
+            HTTPServer myServer = new HTTPServer(directory, "192.168.1.120", 3001);
+            myServer.Start();
+        }
+    }
+}
